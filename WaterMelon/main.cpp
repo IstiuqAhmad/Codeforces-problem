bfcd66fdd64f868c8a21302ece1dc76defdba4ba
Istiuq Ahmad <65025050+IstiuqAhmad@users.noreply.github.com>
2022-03-19 15:51:52 +0600

    Add files via upload

---
+++ WaterMelon/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+   string txt;
+   int n;
+
+   cout<<"Enter your word:";
+   cin>>txt;
+
+   if(txt.length()>10){
+
+    cout<<txt[0]<<txt;
+   }
+
+
+}
